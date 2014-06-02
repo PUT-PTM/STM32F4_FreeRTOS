a81b2930d0f239b9f321496117cceff5abcbb26a
ggoral111 <ggoral111@gmail.com>
2014-06-02 19:05:24 +0200

    02/06/2014

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-FreeRTOS na STM32F4
+FreeRTOS na STM32F4DISCOVERY
+
+*Założenia
+
+Projekt został zrealizowany na potrzeby zajęć laboratoryjnych z przedmiotu Podstawy Techniki Mikroprocesorowej (Informatyka na Wydziale Elektrycznym Politechniki Poznańskiej).
+
+Ogólnymi założeniami projektu było poznanie systemu FreeRTOS i oprogramowanie z jego użyciem mikrokontrolera STM32F4DISCOVERY.
+
+*Cele
+
+Podstawowym celem było poznanie składni i działania najważniejszych elementów systemu FreeRTOS takich jak: tworzenie, uruchamianie, opóźnianie, wstrzymywanie, wznawianie, usuwanie oraz zmiana priorytetu zadań. Założeniami dalszymi było stworzenie kilku zadań mających wykonywać operacje na diodach LED.
+
+W fazie finalnej projekt posiada 5 zadań:
+1) Zapalanie i gaszenie diody,
+2) Zapalenie i gaszenie diody po opóźnieniu rozpoczęcia działania programu (5 sekund),
+3) Wykonanie sekwencji zapalania i gaszenia diód (wywoływane losowo, zadanie usuwane po kilku wykonaniach),
+4) Zapalenie jednej diody po naciśnięciu przycisku i zgaszenie innych (zmiana prorytetów zadań pod wpływem przycisku) oraz wywołanie odpowiedniej sekwencji diód po puszczeniu przycisku,
+5) Zapalanie i gaszenie diód po osiągnięciu przez akcelerometr odpowiedniej wartości (zawieszenie określonych zadań zadań pod wpływem wartości odczytanej z akcelerometru)
+
+*Realizacja
+
+Projekt napisany przy użyciu języka programowania C oraz środowiska CooCox IDE. Kod był testowany na mikrokontrolerze STM32F4DISCOVERY.
+
+* Wykonanie
+
+Jakub Podgórski
+Marcin Kwaśnik
+
--- main/#03/main.c
+++ main/#03/main.c
@@ -3,6 +3,7 @@
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_tim.h"
 #include "misc.h"
+
 #include "FreeRTOS/include/FreeRTOS.h"
 #include "FreeRTOS/include/task.h"
 #include "FreeRTOS/include/semphr.h"
@@ -13,7 +14,7 @@ uint64_t tickTime=0;        // Counts OS ticks (default = 1000Hz).
 
 int dioda_2_priority=0;
 
-// Uchwyty do zadañ
+//Uchwyty do zadañ
 xTaskHandle xHandle_1;
 xTaskHandle xHandle_3;
 xTaskHandle xHandle_4;
@@ -28,6 +29,9 @@ uint32_t LIS302DL_TIMEOUT_UserCallback()
 int8_t acc_z;
 
 void GPIOInit(void);
+void GPIOInit_PWM(void);
+void TIM4_Init(void);
+void TIM4_IRQHandler(void);
 void diody(int x);
 void RNG_Config(void);
 void SPIInit(void);
@@ -36,20 +40,181 @@ void Przycisk_1 (void * pvparameters);
 void Dioda_2 (void * pvparameters);
 void Dioda_3 (void * pvparameters);
 void Dioda_4 (void * pvparameters);
-void Dioda_Random_5 (void * pvparameters);
+void Dioda_Rand_5 (void * pvparameters);
 void Akcelerometr_6 (void * pvparameters);
 
 int main(void)
 {
-	// Inicjalizacje
+
+	//#define xTaskCreate( pvTaskCode, pcName, usStackDepth, pvParameters, uxPriority, pxCreatedTask )
+
+	/*vATask,		// pvTaskCode - the function that implements the task.
+	"ATask",	// pcName - just a text name for the task to assist debugging.
+	100,		// usStackDepth	- the stack size DEFINED IN WORDS.
+	NULL,		// pvParameters - passed into the task function as the function parameters.
+	( 1UL | portPRIVILEGE_BIT ),// uxPriority - task priority, set the portPRIVILEGE_BIT if the task should run in a privileged state.
+	cStackBuffer,// puxStackBuffer - the buffer to be used as the task stack.
+
+	void vTaskDelete( xTaskHandle xTaskToDelete ) PRIVILEGED_FUNCTION;
+
+	======================================================
+
+	 // Block for 500ms.
+ const portTickType xDelay = 500 / portTICK_RATE_MS;
+
+	 for( ;; )
+	 {
+		 // Simply toggle the LED every 500ms, blocking between each toggle.
+		 vToggleLED();
+		 vTaskDelay( xDelay );
+	 }
+ }
+
+ 	 ======================================================
+
+ 	  // Perform an action every 10 ticks.
+ void vTaskFunction( void * pvParameters )
+ {
+ portTickType xLastWakeTime;
+ const portTickType xFrequency = 10;
+
+	 // Initialise the xLastWakeTime variable with the current time.
+	 xLastWakeTime = xTaskGetTickCount ();
+	 for( ;; )
+	 {
+		 // Wait for the next cycle.
+		 vTaskDelayUntil( &xLastWakeTime, xFrequency );
+
+		 // Perform action here.
+	 }
+ }
+	void vTaskDelayUntil( portTickType * const pxPreviousWakeTime, portTickType xTimeIncrement ) PRIVILEGED_FUNCTION;
+
+	==========================================================
+
+	 void vAFunction( void )
+ {
+ xTaskHandle xHandle;
+
+	 // Create a task, storing the handle.
+	 xTaskCreate( vTaskCode, "NAME", STACK_SIZE, NULL, tskIDLE_PRIORITY, &xHandle );
+
+	 // ...
+
+	 // Use the handle to obtain the priority of the created task.
+	 // It was created with tskIDLE_PRIORITY, but may have changed
+	 // it itself.
+	 if( uxTaskPriorityGet( xHandle ) != tskIDLE_PRIORITY )
+	 {
+		 // The task has changed it's priority.
+	 }
+
+	 // ...
+
+	 // Is our priority higher than the created task?
+	 if( uxTaskPriorityGet( xHandle ) < uxTaskPriorityGet( NULL ) )
+	 {
+		 // Our priority (obtained using NULL handle) is higher.
+	 }
+ }
+
+ 	 ======================================================
+
+ 	  void vAFunction( void )
+ {
+ xTaskHandle xHandle;
+
+	 // Create a task, storing the handle.
+	 xTaskCreate( vTaskCode, "NAME", STACK_SIZE, NULL, tskIDLE_PRIORITY, &xHandle );
+
+	 // ...
+
+	 // Use the handle to raise the priority of the created task.
+	 vTaskPrioritySet( xHandle, tskIDLE_PRIORITY + 1 );
+
+	 // ...
+
+	 // Use a NULL handle to raise our priority to the same value.
+	 vTaskPrioritySet( NULL, tskIDLE_PRIORITY + 1 );
+ }
+
+void vTaskPrioritySet( xTaskHandle xTask, unsigned portBASE_TYPE uxNewPriority ) PRIVILEGED_FUNCTION;
+
+	===========================================================
+
+	 void vAFunction( void )
+ {
+ xTaskHandle xHandle;
+
+	 // Create a task, storing the handle.
+	 xTaskCreate( vTaskCode, "NAME", STACK_SIZE, NULL, tskIDLE_PRIORITY, &xHandle );
+
+	 // ...
+
+	 // Use the handle to suspend the created task.
+	 vTaskSuspend( xHandle );
+
+	 // ...
+
+	 // The created task will not run during this period, unless
+	 // another task calls vTaskResume( xHandle ).
+
+	 //...
+
+
+	 // Suspend ourselves.
+	 vTaskSuspend( NULL );
+
+	 // We cannot get here unless another task calls vTaskResume
+	 // with our handle as the parameter.
+ }
+
+	void vTaskSuspend( xTaskHandle xTaskToSuspend ) PRIVILEGED_FUNCTION;
+
+	==============================================================
+
+	 void vAFunction( void )
+ {
+ xTaskHandle xHandle;
+
+	 // Create a task, storing the handle.
+	 xTaskCreate( vTaskCode, "NAME", STACK_SIZE, NULL, tskIDLE_PRIORITY, &xHandle );
+
+	 // ...
+
+	 // Use the handle to suspend the created task.
+	 vTaskSuspend( xHandle );
+
+	 // ...
+
+	 // The created task will not run during this period, unless
+	 // another task calls vTaskResume( xHandle ).
+
+	 //...
+
+
+	 // Resume the suspended task ourselves.
+	 vTaskResume( xHandle );
+
+	 // The created task will once again get microcontroller processing
+	 // time in accordance with it priority within the system.
+ }
+
+	void vTaskResume( xTaskHandle xTaskToResume ) PRIVILEGED_FUNCTION;
+
+	==========================================================================
+	*/
+
+	//Inicjalizacje
 	SystemInit();
 	SystemCoreClockUpdate();
 	GPIOInit();
 	SPIInit();
+	//Inicjalizacja Random
 	RNG_Config();
 	
-	// Tworzenie zadañ
-	// xTaskCreate( pvTaskCode, pcName, usStackDepth, pvParameters, uxPriority, pxCreatedTask )
+	//Tworzenie zadañ
+	//xTaskCreate( pvTaskCode, pcName, usStackDepth, pvParameters, uxPriority, pxCreatedTask )
 
 	xTaskCreate( Przycisk_1, ( signed char * ) "Dioda_1", 100, NULL, 2, &xHandle_1 );
 
@@ -59,7 +224,7 @@ int main(void)
 
 	xTaskCreate( Dioda_4, ( signed char * ) "Dioda_4", 100, NULL, 3 | portPRIVILEGE_BIT, &xHandle_4 );
 
-	xTaskCreate( Dioda_Random_5, ( signed char * ) "Dioda_5", 100, NULL, 2, &xHandle_5 );
+	xTaskCreate( Dioda_Rand_5, ( signed char * ) "Dioda_5", 100, NULL, 2, &xHandle_5 );
 
 	xTaskCreate( Akcelerometr_6, ( signed char * ) "Dioda_6", 100, NULL, 1, &xHandle_6 );
 
@@ -73,14 +238,12 @@ int main(void)
 
 void Przycisk_1( void *pvparameters )
 {
-	//Wciœniêcie przycisku powoduje mryganie diody, a nastêpnie uruchomienie zadania Dioda_2
-
 	const portTickType xDelay = 30 / portTICK_RATE_MS;
 	unsigned int z=0;
 	uint8_t wasPressed = 0;
 
-	while(1)
-	{
+		for (;;){
+
 			while (GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0)==1)
 			{
 				//Zwiêkszenie priorytetu zadania Przycisk_1
@@ -96,17 +259,16 @@ void Przycisk_1( void *pvparameters )
 			    vTaskSuspend( xHandle_3 );
 			    vTaskSuspend( xHandle_4 );
 			    vTaskSuspend( xHandle_5 );
-
 			    dioda_2_priority=1;
 			}
 
-			if (wasPressed)
-			{
+			if (wasPressed){
 				GPIO_ResetBits(GPIOD, GPIO_Pin_12);
 	    	    wasPressed = 0;
+
 			}
 
-			//OpóŸnienie sprawdzaj¹ce co 30[ms], czy przycisk zosta³ wciœniêty
+			//OpóŸnienie sprawdzaj¹ce co 30[ms], czy przycisk zosta³ wciniêty
 			vTaskDelay(xDelay);
 		}
 }
@@ -115,17 +277,16 @@ void Dioda_2( void *pvparameters )
 {
 	//Sekwencja diód wyœwietlana po wciœniêciu przycisku
 
-	const portTickType xDelay_3 = 1 / portTICK_RATE_MS;
 	const portTickType xDelay_2 = 200 / portTICK_RATE_MS;
 	const portTickType xDelay = 1000 / portTICK_RATE_MS;
 	unsigned int i=0;
 
-	while(1)
-	{
+	while(1){
+
 		if(dioda_2_priority==1)
 		{
-			for(i=0; i<5;i++)
-			{
+		for(i=0; i<5;i++)
+		{
 				GPIO_SetBits(GPIOD, GPIO_Pin_13 | GPIO_Pin_15);
 				vTaskDelay( xDelay_2 );
 				GPIO_ResetBits(GPIOD, GPIO_Pin_13 | GPIO_Pin_15);
@@ -134,18 +295,16 @@ void Dioda_2( void *pvparameters )
 				vTaskDelay( xDelay_2 );
 				GPIO_ResetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_14);
 				vTaskDelay( xDelay_2 );
-			}
-
-			dioda_2_priority=0;
-
-			//Przywrócenie zawieszonych zadañ
-			vTaskResume( xHandle_3 );
-			vTaskResume( xHandle_4 );
-			vTaskDelay( xDelay );
-			vTaskResume( xHandle_5 );
 		}
 
-		vTaskDelay( xDelay_3 );
+		dioda_2_priority=0;
+
+		//Przywrócenie zawieszonych zadañ
+		vTaskResume( xHandle_3 );
+		vTaskResume( xHandle_4 );
+		vTaskDelay( xDelay );
+		vTaskResume( xHandle_5 );
+		}
 	}
 }
 
@@ -195,7 +354,7 @@ void Dioda_4( void *pvparameters )
 	}
 }
 
-void Dioda_Random_5( void *pvparameters )
+void Dioda_Rand_5( void *pvparameters )
 {
 	//Zadanie losowo mrygaj¹ce diodami okreœlon¹ sekwencjê
 
@@ -254,18 +413,18 @@ void Dioda_Random_5( void *pvparameters )
 
 void Akcelerometr_6( void *pvparameters )
 {
-	// Zadanie mrygaj¹ce diodami sekwencjê, gdy oœ z osi¹gnie okreœlon¹ wartoœæ
+	//Zadanie mrygaj¹ce diodami sekwencjê, gdy oœ z osi¹gnie okreœlon¹ wartoœæ
 
 	const portTickType xDelay = 1000 / portTICK_RATE_MS;
 	const portTickType xDelay2 = 1000 / portTICK_RATE_MS;
 	unsigned int i=0;
-	while(1)
-	{
+	while(1){
+
 		LIS302DL_Read(&acc_z, LIS302DL_OUT_Z_ADDR, 1);
 
 		if(acc_z<0)
 		{
-			// Zawieszenie zadañ
+			//Zawieszenie zadañ
 			vTaskSuspend( xHandle_3 );
 			vTaskSuspend( xHandle_4 );
 			vTaskSuspend( xHandle_5 );
@@ -278,7 +437,7 @@ void Akcelerometr_6( void *pvparameters )
 				vTaskDelay( xDelay );
 			}
 
-			// Przywrócenie zawieszonych zadañ
+			//Przywrócenie zawieszonych zadañ
 			vTaskResume( xHandle_3 );
 			vTaskResume( xHandle_4 );
 			vTaskDelay( xDelay2 );
@@ -287,12 +446,12 @@ void Akcelerometr_6( void *pvparameters )
 	}
 }
 
-//************************************KONIEC************************************//
+//******************************************************************************//
 
 //********************************INICJALIZACJE*********************************//
 
-void GPIOInit(void)
-{
+void GPIOInit(void){
+
 		/* GPIOD Periph clock enable */
 		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
@@ -316,8 +475,20 @@ void GPIOInit(void)
 		GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
 
-void SPIInit(void)
-{
+void GPIOInit_PWM(void){
+
+		GPIO_InitTypeDef GPIO_InitStructure;
+		RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD,ENABLE);
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13;
+		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+		GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+		GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+		GPIO_Init(GPIOD,&GPIO_InitStructure);
+}
+
+void SPIInit(void){
+
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
 
@@ -352,6 +523,53 @@ void SPIInit(void)
 		 LIS302DL_InitStruct.Self_Test = LIS302DL_SELFTEST_NORMAL;
 		 // aktualizacja ustawien akcelerometru na podstawie wypelnionej struktury
 		 LIS302DL_Init(&LIS302DL_InitStruct);
+}
+
+void TIM4_Init(void){
+
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4,ENABLE);
+
+	TIM_TimeBaseStructure.TIM_Period = 40;
+	TIM_TimeBaseStructure.TIM_Prescaler = 100;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseInit(TIM4,&TIM_TimeBaseStructure);
+
+	//PWM
+	TIM_OCInitTypeDef TIM_OCInitStructure;
+	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	//TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Enable;
+	TIM_OCInitStructure.TIM_Pulse = 0;
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+	TIM_OC1Init(TIM4,&TIM_OCInitStructure);
+	TIM_OC1PreloadConfig(TIM4,TIM_OCPreload_Enable);
+
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource12, GPIO_AF_TIM4);
+
+	TIM_OC2Init(TIM4,&TIM_OCInitStructure);
+	TIM_OC2PreloadConfig(TIM4,TIM_OCPreload_Enable);
+
+	GPIO_PinAFConfig(GPIOD, GPIO_PinSource13, GPIO_AF_TIM4);
+
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+	TIM_ClearITPendingBit(TIM4,TIM_IT_Update);
+	TIM_ITConfig(TIM4,TIM_IT_Update,ENABLE);
+
+	TIM_Cmd(TIM4,ENABLE);
+
+
+	int delay;
+	for(delay=0;delay<100000;delay++){
+		asm("nop");
+	}
 }
 
 void diody(int x)
@@ -398,23 +616,72 @@ void RNG_Config(void)
 	RNG_Cmd(ENABLE);
 }
 
-//************************************KONIEC************************************//
+/*void TIM4_IRQHandler(void){
 
-//****************************INICJALIZACJE*FreeRTOS****************************//
+		if(TIM_GetITStatus(TIM4,TIM_IT_Update) != RESET)
+		{
+			uint8_t wasPressed = 0;
 
-void vApplicationTickHook( void )
-{
+			while(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_0)==1)
+			{
+				TIM4->CCR1 = 40;
+				TIM4->CCR2 = 40;
+				wasPressed = 1;
+			}
+
+			if (wasPressed){
+
+				TIM4->CCR1 = 5;
+				TIM4->CCR2 = 10;
+				wasPressed = 0;
+			}
+
+			vTaskDelay(30 / portTICK_RATE_MS);
+
+			TIM_ClearITPendingBit(TIM4,TIM_IT_Update);
+		}
+}*/
+
+/*void TIM4_IRQHandler(void){
+
+		if(TIM_GetITStatus(TIM4,TIM_IT_Update) != RESET)
+		{
+			while(current!=40)
+			{
+				TIM4->CCR1 = current;
+				TIM4->CCR2 = current;
+				for(s=0; s<=200000; s++);
+				current++;
+			}
+
+			while(current!=0)
+			{
+					TIM4->CCR1 = current;
+					TIM4->CCR2 = current;
+					for(s=0; s<=200000; s++);
+					current--;
+			}
+			TIM_ClearITPendingBit(TIM4,TIM_IT_Update);
+		}
+}*/
+
+//******************************************************************************//
+
+// This FreeRTOS callback function gets called once per tick (default = 1000Hz).
+// ----------------------------------------------------------------------------
+void vApplicationTickHook( void ) {
     ++tickTime;
 }
 
-void vApplicationIdleHook( void )
-{
+// This FreeRTOS call-back function gets when no other task is ready to execute.
+// On a completely unloaded system this is getting called at over 2.5MHz!
+// ----------------------------------------------------------------------------
+void vApplicationIdleHook( void ) {
     ++u64IdleTicksCnt;
 }
 
-void vApplicationMallocFailedHook( void )
-{
-    configASSERT( 0 );
+// A required FreeRTOS function.
+// ----------------------------------------------------------------------------
+void vApplicationMallocFailedHook( void ) {
+    configASSERT( 0 );  // Latch on any failure / error.
 }
-
-//************************************KONIEC************************************//
